7f260786bcacc83f4a8477248626863186a61eca
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-05-28 16:37:05 +0800

    2025.05.27

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+	publicPath: "./", // GitHub Pages 建議用相對路徑，避免資源抓不到
+});
