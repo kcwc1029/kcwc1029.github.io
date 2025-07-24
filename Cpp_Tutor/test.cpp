690664a5135005ad4c0c461e9059b5794ac877ac
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-07-24 22:29:21 +0800

    2025.07.24

--- Cpp_Tutor/test.cpp
+++ Cpp_Tutor/test.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
+#include <cstdarg>
 using namespace std;
 
-int main() {
-    int a[3][4] = {
-        {1,  2,  3,  4},
-        {5,  6,  7,  8},
-        {9, 10, 11, 12}
-    };
+int aaa = 5; // gloabal
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
-            cout << &a[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    return 0;
+void f(){
+    int aaa = 6; // loacl
+    cout << aaa;
 }
+
+int main() {
+    f();
+    cout << aaa;
+}
+
+
