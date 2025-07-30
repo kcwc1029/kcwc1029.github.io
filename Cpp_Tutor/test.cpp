2303ea7c79c7c70176a610f86e9536d85a101265
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-07-30 22:29:44 +0800

    2025.07.30

--- Cpp_Tutor/test.cpp
+++ Cpp_Tutor/test.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <cstdarg>
 using namespace std;
 
-int aaa = 5; // gloabal
-
-void f(){
-    int aaa = 6; // loacl
-    cout << aaa;
+// 接收一個指向含有 5 個 int 的陣列指標
+void printArray(int (*p)[5]) {
+    for (int i = 0; i < 5; ++i) {
+        cout << (*p)[i] << " "; // 取陣列的值
+        // cout << *p[i] << " "; // 為定義行為
+    }
+    cout << endl;
 }
 
 int main() {
-    f();
-    cout << aaa;
+    int arr[5] = {1, 2, 3, 4, 5};
+    printArray(&arr);  // 傳整個陣列的位址
+    return 0;
 }
-
-
