697c1a6320aa51109b5d4501b68d6a0d4daddc71
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-08-08 22:38:22 +0800

    2025.08.08

--- Cpp_Tutor/test.cpp
+++ Cpp_Tutor/test.cpp
@@ -1,31 +1,10 @@
-#include <iostream>
-using namespace std;
-bool flag = true;
+#include <stdio.h>
 
-bool func(int n){ // n = 49
-    if (n == 1){
-        return false;
-    } 
-
-
-    for (int i=2 ; i<n; i++){
-        if(n % i ==0){
-            return true;
-        }
-    }
-    return false;
+void say_hello() { // Text Segment
+    printf("Hello, world!\n"); // Text Segment
 }
 
-
-int main(){
-    int N = 13;
-    // cin >>N;
-    
-    if (func(N) == true) {
-        cout << "Yes" <<endl;
-    }
-    else{
-        cout << "No" << endl;
-    }
+int main() { // Text Segment
+    say_hello();
     return 0;
 }
