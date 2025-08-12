2561f3c87540aa444879212ad852b8eae5f4f558
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-08-12 22:18:53 +0800

    2025.08.12

--- Cpp_Tutor/test.cpp
+++ Cpp_Tutor/test.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <thread>
+
 using namespace std;
 
-template <typename T>
-T findMax(T* arr, int size) {
-    T max = arr[0];
-    for (int i = 1; i < size; ++i)
-        if (arr[i] > max)
-            max = arr[i];
-    return max;
+void some_function() {
+    cout << "some_function is running in thread " << this_thread::get_id() << endl;
+}
+
+void some_other_function() {
+    cout << "some_other_function is running in thread " << this_thread::get_id() << endl;
 }
 
 
 int main(){
-    float arr[] = {1.5, 3.2, 2.7};
-    cout << findMax(arr, 3) << endl;   // 輸出：3.2
+    thread t1(some_function);  // 建立 t1，擁有一個執行緒
+    thread t2 = move(t1); // 2. 將 t1 的所有權轉移給 t2。t1 現在不再擁有執行緒。
+
+    t1 = thread(some_other_function); // 3. 重新為 t1 建立一個新的執行緒，t1 又重新擁有執行緒了。
+    thread t3 = move(t1); // 5. 將 t2 的所有權轉移給 t3。t2 現在不再擁有執行緒。
+    t2.join();
+    t3.join();
 }
+
