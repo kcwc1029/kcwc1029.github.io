f1918c8011c9ca71d4b2747ab1b69c4f6b98a627
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-08-17 23:13:11 +0800

    2025.08.17

--- Python_Tutor/test.py
+++ Python_Tutor/test.py
@@ -1,12 +1,6 @@
-name = '我今年3歲'
-name_bytes = name.encode('utf-8')
-print(name_bytes)
-
-# -----------------------
-
-tt = name_bytes.decode('utf-8')
-
-
-print(tt)
-# print(name_bytes)         # b'\xe6\x88\x91\xe4\xbb\x8a\xe5\xb9\xb43\xe6\xad\xb2'
-# print(len(name_bytes))    # 13
+num = 0
+while num < 10:
+    num += 1
+    if num % 2 != 0:
+        continue # 如果是奇數，跳過本次迴圈
+    print(num)
