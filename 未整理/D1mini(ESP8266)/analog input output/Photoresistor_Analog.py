e76d99c4959a2fbe5c0d98b4a5ed481b2d6717bb
Wei-Cheng Chen <3331363@gmail.com>
2025-12-27 08:46:09 +0800

    2025.12.27 mac

--- 未整理/D1mini(ESP8266)/analog input output/Photoresistor_Analog.py
+++
@@ -1,9 +0,0 @@
-from machine import ADC
-import utime
-
-adc = ADC(0) # 0就是A0
-
-while True:
-    adc_value = adc.read()
-    print(adc_value)
-    utime.sleep(0.5)
--- 未整理/ESP32/ESP32 to thinkspeak/config.py
+++
@@ -1,5 +0,0 @@
-# wifi連線
-WIFI_SSID="ALHC-guest"
-WIFI_PASSWORD="ALHCguest"
-
-
